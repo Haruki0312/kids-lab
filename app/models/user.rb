507f9e6127e3e2020ec88b3cc5fb9df0967afe02
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-09-10 21:27:32 +0900

    ユーザー情報編集機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, on: :create
+
+  validates :username, presence: true
 end
