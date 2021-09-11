ad6c003f5311827b7d74fdfcf0a08b90cbfa6451
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-09-11 23:32:30 +0900

    worksテーブルマイグレーションファイルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_130136) do
+ActiveRecord::Schema.define(version: 2021_09_11_142547) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username", null: false
@@ -25,4 +25,18 @@ ActiveRecord::Schema.define(version: 2021_09_09_130136) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "works", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "material", null: false
+    t.integer "theme_id", null: false
+    t.integer "days_id", null: false
+    t.integer "grate_id", null: false
+    t.text "explanation", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_works_on_user_id"
+  end
+
+  add_foreign_key "works", "users"
 end
