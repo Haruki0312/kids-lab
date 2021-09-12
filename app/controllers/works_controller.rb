de6590135eb7ca3cc19dd3f74f1cbe021feb2fe5
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-09-12 22:01:56 +0900

    new、createアクション、paramsの定義

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,10 +1,28 @@
 class WorksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
 
   def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to root_path
+    else
+      @work = Work.new(work_params)
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+  def work_params
+    params.require(:work).permit(:image, :title, :material, :theme_id, :days_id, :grade_id, :explanation).merge(user_id: current_user.id)
   end
 end
