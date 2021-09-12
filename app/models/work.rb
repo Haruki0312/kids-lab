1a921a82ad9e456749c801bdbf999b45167ad202
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-09-12 21:39:22 +0900

    バリーでションの記述

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,12 @@
 class Work < ApplicationRecord
-
+  validates :title, presence: true
+  validates :material, presence: true
+  validates :explanation, presence: true
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :theme_id
+    validates :days_id
+    validates :grade_id
+  end
 
   belongs_to :user
   has_one_attached :image
