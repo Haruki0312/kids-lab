e7220a08c7c2973c2f5ac69fad9b280446a1d18c
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-09-12 23:45:11 +0900

    workモデルの単体テスト実装

--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :work do
-    
+    title {'テストタイトル'}
+    material {'テストテスト材料'}
+    theme_id {2}
+    days_id {2}
+    grade_id {2}
+    explanation {'テストテストテスト説明'}
+    association :user
+
+    after(:build) do |work|
+      work.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Work, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @work = FactoryBot.build(:work)
+  end
+
+  describe '作品新規投稿' do
+    context '新規投稿できる場合' do
+      it 'image、title、material、theme_id、days_id、grade_id、explanationが存在すれば登録できる' do
+      expect(@work).to be_valid
+      end
+    end
+
+    context '新規登録できない場合' do
+      it 'imageが空では登録できない' do
+        @work.image = nil
+        @work.valid?
+        expect(@work.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'titleが空では登録できない' do
+        @work.title = ''
+        @work.valid?
+        expect(@work.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'materialが空では登録できない' do
+        @work.material = ''
+        @work.valid?
+        expect(@work.errors.full_messages).to include("Material can't be blank")
+      end
+      it 'theme_idが「1」では登録できない' do
+        @work.theme_id = 1
+        @work.valid?
+        expect(@work.errors.full_messages).to include("Theme can't be blank")
+      end
+      it 'days_idが「1」では登録できない' do
+        @work.days_id = 1
+        @work.valid?
+        expect(@work.errors.full_messages).to include("Days can't be blank")
+      end
+      it 'grade_idが「1」では登録できない' do
+        @work.grade_id = 1
+        @work.valid?
+        expect(@work.errors.full_messages).to include("Grade can't be blank")
+      end
+      it 'explanationが空では登録できない' do
+        @work.explanation = ''
+        @work.valid?
+        expect(@work.errors.full_messages).to include("Explanation can't be blank")
+      end
+    end
+  end
 end
