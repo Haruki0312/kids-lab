edf3309d55d8f3a25005d5f9497368bc40f87724
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-09-15 22:28:02 +0900

    ルーティングとコントローラーの記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,16 @@
 class LikesController < ApplicationController
-  
+  before_action :set_work, only: [:create, :destroy]
+
+  def create
+    @like = Like.create(user_id: current_user.id, work_id: params[:work_id])
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_usr.id, work_id: params[:work_id])
+    like.destroy
+  end
+
+  def set_work
+    @work = Work.find(params[:work_id])
+  end
 end
